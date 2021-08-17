b1b0294a541c8648e8fa0554dcd6eeb38a90afd9
David Mendes <davidgmendes@hotmail.com>
2021-08-17 17:47:14 -0300

    :lipstick: fixando o modal de edicao

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -6,7 +6,7 @@ export const Background = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.7);
-  position: absolute;
+  position: fixed;
   display: flex;
   justify-content: center;
   align-items: center;
